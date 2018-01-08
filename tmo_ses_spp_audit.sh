407826fa295a403e024a960d585510484735409c
Ashish Nagar <ashish.nagar@ericsson.com>
2018-01-08 23:52:55 +0000

    Update tmo_ses_spp_audit.sh

--- tmo_ses_spp_audit.sh
+++ tmo_ses_spp_audit.sh
@@ -2,53 +2,33 @@
 # This script will be used to audit the TMO SES SPP SSM and SDD nodes.
 # Managed by TMO SES SI Team
 #
-logfile=audit_output_`date +%y-%m-%d-%H:%M:%S`.log
 echo " "
-echo "Starting Audit of all the nodes, please check the output file for any errors...."
+echo "Starting Audit on all the nodes, please wait...."
+logfile=audit_output_`date +%y-%m-%d-%H:%M:%S`.log
 echo " " >>$logfile
 echo "Audit Started" >> $logfile
 echo " " >>$logfile
-echo "*********************" >>$logfile
-echo "NSCTRL services status" >>$logfile
-echo "*********************" >>$logfile
+# Function to call the ansible commands
+
+audit () { 		
 echo " " >>$logfile
-ansible sesssm -m shell -a 'nsctrl status' -i ../tmo-nsim-ses-ansible-inventory/ >>$logfile 2>&1
+echo "***************************************************************************" >>$logfile
+echo " $2 on $1" >>$logfile
+echo "***************************************************************************" >>$logfile
 echo " " >>$logfile
-echo "**********************" >>$logfile
-echo "SSM ESA Cluster Status" >>$logfile
-echo "**********************" >>$logfile
-echo " " >>$logfile
-ansible sesssm -m shell -a '/usr/local/esa/bin/esaclusterstatus' -i ../tmo-nsim-ses-ansible-inventory/ >>$logfile 2>&1
-echo " " >>$logfile
-echo "**********************" >>$logfile
-echo "SSM FMACTIVEALARMS" >>$logfile
-echo "**********************" >>$logfile
-echo " " >>$logfile
-ansible sesssm -m shell -a '/usr/local/esa/bin/fmactivealarms' -i ../tmo-nsim-ses-ansible-inventory/ >>$logfile 2>&1
-echo " " >>$logfile
-echo "**********************" >>$logfile
-echo "CONSUL MEMBERS" >>$logfile
-echo "**********************" >>$logfile
-echo " " >>$logfile
-ansible sesssm -m shell -a 'consul members' -i ../tmo-nsim-ses-ansible-inventory/ >>$logfile 2>&1
-echo " " >>$logfile
-echo "**********************" >>$logfile
-echo "SES Troubleshooter logs" >>$logfile
-echo "**********************" >>$logfile
-echo " " >>$logfile
-ansible sests -m shell -a 'pallogviewer -20 /var/log/miep/troubleshooter_log.xml |grep Text' -i ../tmo-nsim-ses-ansible-inventory/ >>$logfile 2>&1
-echo " " >>$logfile
-echo "**********************" >>$logfile
-echo "SPP Troubleshooter logs" >>$logfile
-echo "**********************" >>$logfile
-echo " " >>$logfile
-ansible sppts -m shell -a 'pallogviewer -20 /var/log/miep/troubleshooter_log.xml |grep Text' -i ../tmo-nsim-ses-ansible-inventory/ >>$logfile 2>&1
-echo " " >>$logfile
-echo "**********************" >>$logfile
-echo "SDD Cluster status" >>$logfile
-echo "**********************" >>$logfile
-echo " " >>$logfile
-ansible sessdd -m shell -a 'crm_mon -Afr -1' -i ../tmo-nsim-ses-ansible-inventory/ >>$logfile 2>&1
+ansible $1 -m shell -a "$2" -i ../tmo-nsim-ses-ansible-inventory/ >>$logfile 2>&1
+}
+
+# Calling function to audit the systems
+
+audit all 'nsctrl status'
+audit sesssm '/usr/local/esa/bin/esaclusterstatus'
+audit sesssm '/usr/local/esa/bin/fmactivealarms'
+audit sesssm 'consul members'
+audit sests 'pallogviewer -20 /var/log/miep/troubleshooter_log.xml |grep Text'
+audit sppts 'pallogviewer -20 /var/log/miep/troubleshooter_log.xml |grep Text'
+audit sessdd 'crm_mon -Afr -1'
+
 echo " "
 echo "Audit Completed. Make sure to check $logfile for detailed output and errors"
 echo " "
