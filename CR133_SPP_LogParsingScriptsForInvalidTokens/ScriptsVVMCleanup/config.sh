5aeff7df7495771ace4efdbf0b8a9c2bb137d95a
lmcfrha <frederic.hanslik@ericsson.com>
2018-06-19 13:29:55 -0400

    Limit the max num of deletion per script execution to 10000. Automatically update the IP of DB.

--- CR133_SPP_LogParsingScriptsForInvalidTokens/ScriptsVVMCleanup/config.sh
+++ CR133_SPP_LogParsingScriptsForInvalidTokens/ScriptsVVMCleanup/config.sh
@@ -15,3 +15,17 @@ DATE=`date +%Y-%m-%d`
 APPLOGFOLDER="./logs"
 LOGRETENTION='30'
 BACKLOGINITIAL='240'
+#
+# Find out the DB IP and user passwd
+#
+databaseIp=`grep dmsDbConnectionString /opt/miep/etc/config/ConfigData.json | grep -oE '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' | head -1`
+port=`grep dmsDbConnectionString /opt/miep/etc/config/ConfigData.json | awk -F')' '{print $5}' | tr -dc '0-9'`
+dbuser=`grep -w dmsDbApplicationId /opt/miep/etc/config/ConfigData.json | awk -F'"' '{print $4}'`
+dbpass=`grep -w dmsDbApplicationIdPassword /opt/miep/etc/config/ConfigData.json | awk -F'"' '{print $4}'`
+#
+# Update the datasource.properties with values determined by the preceding commands
+#
+sed -i s/^DB_URL=.*/DB_URL=jdbc:oracle:thin:${dbuser}\\/${dbpass}@${databaseIp}:${port}\\/DESPDB/ datasource.properties
+sed -i s/^USER=.*/USER=${dbuser}/ datasource.properties
+sed -i s/^PASS=.*/PASS=${dbpass}/ datasource.properties
+
